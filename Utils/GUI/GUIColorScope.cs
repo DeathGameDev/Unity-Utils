9c06b40d84abe190f1069a5d36b729a707d72050
Mckenon <MckenonDev@gmail.com>
2020-04-20 19:20:55 -0500

    Added GUIColorScope

---
+++ Utils/GUI/GUIColorScope.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class GUIColorScope : Scope
+{
+	private readonly Color cachedColor;
+
+	public GUIColorScope(Color color)
+	{
+		cachedColor = GUI.color;
+		GUI.color = color;
+	}
+
+	protected override void CloseScope()
+	{
+		GUI.color = cachedColor;
+	}
+}
